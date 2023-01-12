ea5f2f77a2bf86a6ad91d5a8327c6b460e97c9db
akshithku <akshithkumar.karla@gmail.com>
2023-01-12 16:55:20 +0530

    protrips-lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/Navbar"
+import Contacts from "./component/Contacts"
+import Home from "./component/Home"
+import Form from "./component/Form"
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/1" element={<Contacts/>} />
+          <Route path="/2" element={<Form/>} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
